5a15097ecfc75afb5b44eda2276fc44eceb71827
SRIVARDHAN BABA VEMULA <srivardhanbabavemula@gmail.com>
2026-04-17 18:11:43 -0400

    Create staging model for treatments data

---
+++ dbt_project/models/staging/stg_treatments.sql
@@ -0,0 +1,18 @@
+
+
+with source as (
+    select * from {{ source('hospital_raw', 'treatments') }}
+)
+
+select
+    treatment_id,
+    appointment_id,
+    treatment_type,
+    description,
+    cost::decimal(10,2)                      as cost,
+    treatment_date::date                     as treatment_date,
+    to_char(treatment_date::date, 'YYYYMMDD')::int as date_key
+from source
+where treatment_id     is not null
+  and appointment_id   is not null
+  and cost             >= 0
